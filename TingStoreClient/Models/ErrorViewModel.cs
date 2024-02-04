b7d3be429180393f5eac0f4e681063fcb4f47fe4
BCvuongvan <vuongvann006@gmail.com>
2024-02-04 12:21:31 +0700

    Create project SWP

---
+++ TingStoreClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TingStoreClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
