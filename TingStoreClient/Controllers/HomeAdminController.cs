87417609fdd3e782979506428ea48f8fd2baab1d
BCvuongvan <vuongvann006@gmail.com>
2024-02-05 21:08:49 +0700

    set main

--- TingStoreClient/Controllers/HomeAdminController.cs
+++ TingStoreClient/Controllers/HomeAdminController.cs
@@ -1,26 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using TingStoreClient.Models;
 
 namespace TingStoreClient.Controllers
 {
-    public class HomeController : Controller
+    public class HomeAdminController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-
     }
 }
--- TingStoreClient/Controllers/UserManagementController.cs
+++ TingStoreClient/Controllers/UserManagementController.cs
@@ -1,26 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using TingStoreClient.Models;
 
 namespace TingStoreClient.Controllers
 {
-    public class HomeController : Controller
+
+    public class UserManagementController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
 
     }
 }
