e7b5bdd86299f328e708f47191332afe431d3480
DATDT1810 <147079917+DATDT1810@users.noreply.github.com>
2024-02-22 13:38:03 +0700

    Create Manage Order function

---
+++ TingStoreAPI/Controllers/OrderDetailController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TingStoreAPI.DB;
+
+namespace TingStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderDetailController : ControllerBase
+    {
+        private readonly ApplicationDBContext _db;
+
+        public OrderDetailController(ApplicationDBContext db)
+        {
+            this._db = db;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllOrderDetail(int orderId)
+        {
+            var orderDetails = _db.orderDetails
+                .Include(od => od.order)
+                .Include(od => od.product)
+                .FirstOrDefault(od => od.orderId == orderId);
+            if (orderDetails == null)
+            {
+                return NotFound("No order details found for the given order ID.");
+            }
+            return Ok(orderDetails);
+        }
+        
+    }
+}
--- TingStoreClient/Controllers/HomeAdminController.cs
+++ TingStoreClient/Controllers/HomeAdminController.cs
@@ -2,17 +2,53 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using TingStoreClient.Models;
 
 namespace TingStoreClient.Controllers
 {
     public class HomeAdminController : Controller
     {
 
-        public IActionResult Index()
+        private readonly HttpClient client = null;
+
+        private string orderApi;
+
+        public HomeAdminController()
         {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            this.orderApi = "https://localhost:5001/api/Order";
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            int countOrder = 0;
+            HttpResponseMessage response = await client.GetAsync(orderApi);
+            string data = await response.Content.ReadAsStringAsync();
+
+            // var option = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+            var options = new JsonSerializerOptions()
+            {
+                NumberHandling = JsonNumberHandling.AllowReadingFromString |
+    JsonNumberHandling.WriteAsString
+            };
+            List<Order> listOrder = JsonSerializer.Deserialize<List<Order>>(data, options);
+            foreach (var item in listOrder)
+            {
+                if (item.orderStatusId == 1)
+                {
+                    countOrder++;
+                }
+            }
+            ViewBag.countOrder = countOrder;
             return View();
         }
     }
