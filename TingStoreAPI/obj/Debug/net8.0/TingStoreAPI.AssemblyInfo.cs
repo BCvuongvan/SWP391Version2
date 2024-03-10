50d6385be804108a2129540b3dedd75da52d6618
marunhy <139757591+marunhy@users.noreply.github.com>
2024-03-10 14:43:28 +0700

    search function

--- TingStoreAPI/obj/Debug/net8.0/TingStoreAPI.AssemblyInfo.cs
+++ TingStoreAPI/obj/Debug/net8.0/TingStoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TingStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07229c41405ed26cc4e827654315094e5cb8eaa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TingStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TingStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
