39bec93e2b95456895aa9f3ecfb0db024a1f4580
DATDT1810 <147079917+DATDT1810@users.noreply.github.com>
2024-03-11 21:35:57 +0700

    Modified all UI project

--- TingStoreClient/obj/Debug/net8.0/TingStoreClient.AssemblyInfo.cs
+++ TingStoreClient/obj/Debug/net8.0/TingStoreClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TingStoreClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140cbef71c79565711c725ef9b0ae5e275a8ef14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50bc03357100ceeed09ed8587643fea5fb924f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("TingStoreClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TingStoreClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
